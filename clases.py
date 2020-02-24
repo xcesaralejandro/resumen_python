9f71de3cb793cee0af094d2ea36ba3fbaf9d34e3
César Mora Cid <cesar.mcid@gmail.com>
2020-02-24 09:38:33 -0300

    Definición teorica de clases

---
+++ clases.py
@@ -0,0 +1,8 @@
+# CLASES
+
+# Una clase es una plantilla común para la creación de objetos.
+# Los objetos son los elementos diferentes entre si pero comparten la misma clase.
+
+# EJ: Una clase puede ser humano, mientras que algunos objetos pueden ser hombre y mujer.
+# En la practica clases y objetos se trabajan como lo mismo (class) pero este concepto de clases y objetos 
+# nos permitirán modelar mejor los conceptos (Mediante herencia) y así trabajar más modularizado. 
